7dd35e2a8978e942ce95260c027cc85791e643c1
Adnan Kasim <adnankasim8@gmail.com>
2019-07-30 11:37:32 +0800

    Perbaikan Fitur Daftar

--- database/migrations/2019_03_12_094805_create_pengguna_table.php
+++ database/migrations/2019_03_12_094805_create_pengguna_table.php
@@ -15,7 +15,7 @@ class CreatePenggunaTable extends Migration
             $table->string('password_pengguna', 100);
             $table->enum('jenis_kelamin', ['perempuan', 'laki-laki']);
             $table->string('no_hp', 15);
-            $table->string('foto_pengguna', 100)->nullable();
+            $table->string('foto_pengguna', 100)->default('');
             $table->timestamps();
         });
 
